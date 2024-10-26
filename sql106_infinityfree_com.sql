be5f411cc690c500d275997f0cf87cf2e8490478
jaygajiparaoceaninfotech <jaygajiparaoceaninfotech@gmail.com>
2024-10-26 15:39:39 +0530

    Add files via upload

---
+++ sql106_infinityfree_com.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: sql106.infinityfree.com
+-- Generation Time: Oct 26, 2024 at 06:06 AM
+-- Server version: 10.6.19-MariaDB
+-- PHP Version: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- $servername = "sql106.infinityfree.com";
+-- $username = "if0_37558182";
+-- $password = "FJfhKsd2b7m";
+-- $database='if0_37558182_ocean';
+
+-- // Create connection
+-- $conn = mysqli_connect($servername, $username, $password,$database);
+
+-- // Check connection
+-- if (!$conn) {
+--   die("Connection failed: " . mysqli_connect_error());
+-- }
+
